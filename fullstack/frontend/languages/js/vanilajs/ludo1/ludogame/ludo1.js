c3f694e940e77068937bb5a2ce6a9e75ac589945
pranu-08 <78089848+pranu-08@users.noreply.github.com>
2021-09-14 16:37:19 +0530

    iskl

---
+++ fullstack/frontend/languages/js/vanilajs/ludo1/ludogame/ludo1.js
@@ -0,0 +1,19 @@
+// function foo() {
+
+//     var player2 = Math.floor(Math.random() * 6) + 1;
+//     var play2 = 'images/dice' + player2 + '.jpg';
+//     document.getElementById('dice2').setAttribute('src', play2);
+
+//     var player1 = Math.floor(Math.random() * 6) + 1;
+//     var play1 = 'images/dice' + player1 + '.jpg';
+//     document.getElementById('dice1').setAttribute('src', play1);
+
+
+//     if (player1 > player2) {
+//         document.getElementById('heading').innerHTML = 'Player1 WIN :)'
+//     } else if (player2 > player1) {
+//         document.getElementById('heading').innerHTML = 'Player2 WIN :)'
+//     } else {
+//         document.getElementById('heading').innerHTML = 'DRAW'
+//     }
+// }
